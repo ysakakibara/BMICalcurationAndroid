d966bde292218d7853b2ba679041a18d02bcc20d
サンケイ クリエス <cries@mac-mini-2011srv2.local>
2020-01-09 17:31:46 +0900

    Fragmentに切り出す

--- app/src/main/java/com/example/bmiapplication/MainActivity.kt
+++ app/src/main/java/com/example/bmiapplication/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.bmiapplication
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.appcompat.app.AlertDialog
+import androidx.fragment.app.Fragment
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -11,6 +12,10 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-
+        if (savedInstanceState == null) {
+            val transaction = supportFragmentManager.beginTransaction()
+//                transaction.add(R.id.calcurationFragment )
+                transaction.commit()
+        }
     }
 }
